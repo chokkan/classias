346ac0e166ba5decc2513e5d90dff727ca5a8851
naoaki <naoaki@b1289102-c212-4d89-882f-30837ae6af19>
2009-09-25 08:56:27 +0000

    Preparation for version 1.0.
    git-svn-id: file:///home/svnrepos/software/classias/trunk@181 b1289102-c212-4d89-882f-30837ae6af19

--- include/classias/version.h
+++ include/classias/version.h
@@ -35,7 +35,7 @@
 
 #define	CLASSIAS_NAME           "Classias"
 #define	CLASSIAS_COPYRIGHT      "Copyright (c) 2008,2009 Naoaki Okazaki"
-#define	CLASSIAS_MAJOR_VERSION  0
-#define CLASSIAS_MINOR_VERSION  7
+#define	CLASSIAS_MAJOR_VERSION  1
+#define CLASSIAS_MINOR_VERSION  0
 
 #endif/*__CLASSIAS_VERSION_H__*/
